eeeaee718f730713f9958ce5cec9a5692662233c
Jenan Mannette <jenanmannette@gmail.com>
2015-09-14 19:35:39 -0600

    assessment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var timestamp = moment().format('MM/DD/YY');
 
 var db = require('monk')('localhost/jobs');
 var postings = db.get('postings');
@@ -9,48 +8,73 @@ var postings = db.get('postings');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     postings.find({}, function (err, docs) {
-        res.render('index', {postings: docs, title: 'Jobsly.io'})
+        res.render('index', {postings: docs})
     })
 });
 
-//new job posting
+// ------- new job posting ------- //
 router.get('/postings/new', function (req, res, next) {
     res.render('new');
 });
 
-//post
+// ------- post ------- //
 router.post('/', function (req, res, next) {
+    req.body.applicants = [];
     postings.insert(req.body);
-    //postings.update({_id: req.params.id}, {'$set': {'timestamp': timestamp}});
-        res.redirect('/');
+    res.redirect('/');
 });
 
-//show job posting
+// ------- show job posting ------- //
 router.get('/postings/:id', function (req, res, next) {
     postings.findOne({_id: req.params.id}, function (err, docs) {
         res.render('show', {postings: docs});
     });
 });
 
-//edit job posting
+// ------- edit job posting ------- //
 router.get('/postings/:id/edit', function (req, res, next) {
     postings.findOne({_id: req.params.id}, function (err, docs) {
-       res.redirect('/'); 
+       res.render('edit', docs);
     });
 });
 
-//update job posting
+// ------- update job posting ------- //
 router.post('/postings/:id/update', function (req, res, next) {
-    postings.findOne({_id: req.params.id}, req.body, function (err, docs) {
+    postings.update({_id: req.params.id}, req.body, function (err, docs) {
         res.redirect('/');
     });
 });
 
-//add applicant
+// ------- add applicant form ------- //
+router.get('/postings/:id/applicants', function (req, res, next) {
+    res.render('applicants');
+});
 
-//delete applicant
+ //--------------------------- NESTED ROUTES --------------------------- //
 
-//delete job posting
+// ------- add applicant ------- //
+router.post('/postings/:id/applicants', function (req, res, next) {
+    postings.findOne({_id: req.params.id}, function (err, docs) {
+        if (err) res.send('No applicantInfo');
+        docs.applicants.push(req.body);
+        postings.update({_id: req.params.id}, docs, function (err, docs) {
+            res.redirect('/postings/' + req.params.id);
+        });
+    });
+});
+
+// ------- delete applicant ------- //
+router.post('/postings/:id/applicants/delete', function (req, res, next) {
+    postings.findOne({_id: req.params.id}, function (err, docs) {
+        var index = docs.applicants.indexOf(req.body.applicantInfo);
+        docs.applicants.splice(index,1);
+        postings.update({_id: req.params.id}, docs, function (err, docs) {
+            res.redirect('/postings/' + req.params.id);
+        });
+    });
+});
+
+// ------- delete job posting ------- //
 router.post('/postings/:id/delete', function (req, res, next) {
     postings.remove({_id: req.params.id}, function (err, docs) {
         res.redirect('/');
